35d6f21bef5687ef6ab80869c9d7368d4080200a
Md Nadeem <nadeem.nad786@gmail.com>
2023-05-04 19:04:41 +0530

    test commit

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -11,9 +11,10 @@ export class HttpService {
 
   // private url =
   //   'https://firebasestorage.googleapis.com/v0/b/scripbox-4d466.appspot.com/o/ideas.json?alt=media&token=96f9f96d-5b36-4a15-94aa-bc9fea8bb352';
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   get(): Observable<any> {
+
     return this.http.get(this.url);
   }
 
